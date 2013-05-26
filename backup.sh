a1b62fdb9ae3817941f550b021c4f48c563c28d6
Jordan Byron <jordan.byron@gmail.com>
2013-05-26 11:38:28 -0400

    Use "current" dir on server

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
-cd /home/deploy/backup
+cd /home/deploy/backup/current
 source config/settings
 BACKUP_FILE="`date \+\\%Y_\\%m_\\%d_\\%H_\\%M`.sql.bz2"
 pg_dump --clean --no-owner --no-privileges -h localhost -U momma momma-production | bzip2 > $BACKUP_FILE
