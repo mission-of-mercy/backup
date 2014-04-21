8d745ad63577c5620b1b80680cef6b0ab9507f1c
Jordan Byron <jordan.byron@gmail.com>
2014-04-20 21:19:39 -0400

    Yay CT server overrides

--- config/deploy/ct.rb
+++ config/deploy/ct.rb
@@ -1,1 +1,5 @@
-server "momma.ct", :app, :web, :db, :primary => true
+require './lib/capistrano_local_overrides'
+
+locally_overridable "momma:ct" do
+  server "momma.ct", :app, :web, :db, :primary => true
+end
